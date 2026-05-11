eca3536079cad80905945b15bcf97606fc121af4
ItsLewis-DE <thanhphong1110n@gmail.com>
2026-05-11 07:40:51 +0200

    hiih

--- dags/backend/extract_companies.py
+++ dags/backend/extract_companies.py
@@ -2,11 +2,31 @@ import json
 import pendulum
 import requests
 import os,logging
+from dotenv import load_dotenv
+
 def extract_companies():
+    logger = logging.getLogger(__name__)
+    load_dotenv('/usr/local/env')
     exchanges = ['NYSE', 'NASDAQ', 'NYSEMKT', 'NYSEARCA', 'OTC', 'BATS', 'INDEX']
+    result = []
+    total =0
     for exchange in exchanges:
-        params = {
-            'exchange': exchange,
-            'token':
-        }
-        url = 'https://api.sec-api.io/mapping/exchange/nasdaq?token=YOUR_API_KEY'
+        try:
+            params = {
+                'exchange': exchange,
+                'token': os.getenv('API_COMPANIES')
+            }
+            url = 'https://api.sec-api.io/mapping/exchange/nasdaq?token=YOUR_API_KEY'
+            response = requests.get(url,params = params,retry = 30)
+            response.raise_for_status()
+            data = response.json()
+        except requests.exceptions.RequestException as e:
+            logger.exception('There are an error while extracting data from API!')
+            raise
+        result.extend(data)
+        total += len(data)
+    date = pendulum.now(tz='Aisa/Ho_Chi_Minh').strftime("%Y_%m_%d")
+    path = f'/usr/local/data/raw/companies/raw_companies_{date}.json'
+    with open(path,'w',encoding = 'utf-8') as file:
+        json.dump(result,file,indent=2)
+    logging.info(f"Extracting {total} successffuly!")
