e9c2de48c941c7d0793e4a3b91fafa4c41b3da20
ItsLewis-DE <thanhphong1110n@gmail.com>
2026-05-15 10:18:53 +0200

    quamet

--- dags/elt/transform/transform_parquet_1.py
+++ dags/elt/transform/transform_parquet_1.py
@@ -14,7 +14,7 @@ import snowflake.connector
 def trans_par(tablename,columns,col_conflict):
     load_dotenv('/usr/local/.env')
     logger = logging.getLogger(__name__)
-    current_date = pendulum.now(tz='Asia/Ho_Chi_Minh').subtract(days=1).strftime("%Y_%m_%d")
+    current_date = pendulum.now(tz='Asia/Ho_Chi_Minh').strftime("%Y_%m_%d")
     logger.info("Connecting to snowflake!")
     conn = snowflake.connector.connect(
         user=os.getenv("SNOWFLAKE_USER"),
@@ -37,15 +37,15 @@ def trans_par(tablename,columns,col_conflict):
         MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
         FILE_FORMAT = (TYPE = PARQUET);
     """
-    columns_string = ', '.join([f"target.{col} = source.{col}" for col in columns])
-    columns_insert_string =', '.join([f"{col}" for col in columns])
-    columns_value_string = ', '.join([f"source.{col}" for col in columns])
-    col_conflict = [f"target.{col} = source.{col}" for col in col_conflict]
-    col_conflict = ' AND '.join(col_conflict)
+    columns_string = ', '.join([f'target.{col} = source.{col}' for col in columns if col not in col_conflict])
+    columns_insert_string =', '.join([f'{col}' for col in columns])
+    columns_value_string = ', '.join([f'source.{col}' for col in columns])
+    conflict_conditions = [f'target.{col} = source.{col}' for col in col_conflict]
+    col_conflict_string = ' AND '.join(conflict_conditions)
     query_merge = f"""
         MERGE INTO stock_schema.{tablename} AS target
         USING stock_schema.{tablename}_stg AS source
-        ON {col_conflict}
+        ON {col_conflict_string}
         WHEN MATCHED THEN
         UPDATE SET
                 {columns_string}
@@ -60,43 +60,52 @@ def trans_par(tablename,columns,col_conflict):
     query_truncate = f"""
             TRUNCATE TABLE stock_schema.{tablename}_stg;
         """
+    query_drop = f"""
+            DROP TABLE stock_schema.{tablename}_stg;
+    """
+
     cursor.execute(query_create_stg)
     cursor.execute(query_copy_into_stage)
     cursor.execute(query_merge)
     cursor.execute(query_truncate)
+    cursor.execute(query_drop)
     conn.commit()
     cursor.close()
     conn.close()
 
 def transform_parquet_1():
-    #trans table company
-    tablename = 'company'
-    col_conflict = ['ticker','cik']
-    columns = ['company_id','company_name','ticker','cik','cusip','exchange_id','isDelisted','industry_id','location','currency','category','sic_code']
-    trans_par(tablename,columns,col_conflict)
+    # #trans table company
+    # tablename = 'company'
+    # col_conflict = ['ticker','cik']
+    # columns = ['company_id','company_name','ticker','cik','cusip','exchange_id','isDelisted','industry_id','location','currency','category','sic_code']
+    # trans_par(tablename,columns,col_conflict)
 
-    #trans table exchange
-    tablename = 'exchange'
-    col_conflict = ['exchange_name']
-    columns = ['exchange_id','exchange_name','region_id']
-    trans_par(tablename,columns,col_conflict)
+    # #trans table exchange
+    # tablename = 'exchange'
+    # col_conflict = ['exchange_name']
+    # columns = ['exchange_id','exchange_name','region_id']
+    # trans_par(tablename,columns,col_conflict)
 
-    # #trans table fama_classification
+    #trans table fama_classification
     # tablename = 'fama_classification'
     # columns = ['fama_id','fama_industry','fama_sector']
-    # trans_par(tablename,columns)
+    # col_conflict = ['fama_industry','fama_sector']
+    # trans_par(tablename,columns,col_conflict)
 
     # #trans table industry
     # tablename = 'industry'
     # columns = ['industry_id','industry_name','sector_name']
-    # trans_par(tablename,columns)
+    # col_conflict = ['industry_name']
+    # trans_par(tablename,columns,col_conflict)
 
     # #trans table news
     # tablename = 'news'
-    # columns = ['title','url','time_published','authors','summary','banner_image','source','category_within_source','source_domain','topics','overall_sentiment_score','overall_sentiment_lable','ticker_sentiment']
-    # trans_par(tablename,columns)
+    # columns = ['title','url','time_published','authors','summary','banner_image','source','category_within_source','source_domain','topics','overall_sentiment_score','overall_sentiment_label','ticker_sentiment']
+    # col_conflict = ['title']
+    # trans_par(tablename,columns,col_conflict)
 
     # #trans table ohlc
-    # table_name = 'ohlc'
-    # columns = ['T','v','vw','o','h','l','t','n']
-    # trans_par(tablename,columns)
+    tablename = 'ohlc'
+    columns = ['T','v','vw','o','c','h','l','t_time','n']
+    col_conflict = ['T','t_time']
+    trans_par(tablename,columns,col_conflict)
